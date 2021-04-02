1a9288b27254944dcdd191bfea31034d35f9ff94
lukkio88 <luca.gagliano.1988@gmail.com>
2021-04-03 00:25:22 +0100

    Added PROJECT_SOURCE_DIR in CMakeLists.txt and simplified code

--- include/line.hpp
+++ include/line.hpp
@@ -2,37 +2,27 @@
 #define LINE_H
 #include <point.hpp>
 
-class Line
+struct Line
 {
 public:
-	Line(const Point& origin, const Vector& direction);
-	Point getOrigin() const;
-	Point getDirection() const;
-	void setOrigin(const Point& origin);
-	void setDirection(const Vector& direction);
 	bool contains(const Point& p) const;
 	Point getIntersection(const Line& l) const;
-private:
-	Point mOrigin;
-	Vector mDirection;
+public:
+	Point origin;
+	Vector direction;
 };
 
 bool doIntersect(const Line& l, const Line& s);
 
-class Segment
+struct Segment
 {
 public:
-	Segment(const Point& origin,const Point& destination);
-	Point getOrigin() const;
-	Point getDestination() const;
 	Point getDirection() const;
-	void setOrigin(const Point& origin);
-	void setDestination(const Point& destination);
 	bool contains(const Point& point);
 	void split(Segment& s1, Segment& s2, const Point& p);
-private:
-	Point mOrigin;
-	Point mDestination;
+public:
+	Point origin;
+	Point destination;
 };
 
 #endif
